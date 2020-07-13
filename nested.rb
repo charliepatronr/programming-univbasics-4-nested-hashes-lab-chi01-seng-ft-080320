3bf8c6e80e5d84da1ceb24ace097683af55b4d42
Juan Carlos Patron <charliepatronr@gmail.com>
2020-07-13 21:38:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -83,6 +83,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[]
 
 end
 
